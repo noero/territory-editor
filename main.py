17715deadeeee420b6ccee8ff17fc67effa704e5
Romain Damiano <rdamianotravail@gmail.com>
2022-05-29 23:31:24 +0200

    wxPython Framework

--- main.py
+++ main.py
@@ -1,15 +1,105 @@
-# from threading import Thread
-from ui import *
+#!/usr/bin/env python
+import io
+import folium
+from folium import plugins
+import wx
+import wx.html2
 
 
-class ThreadedClient:
-    def __init__(self, app):
+class HelloFrame(wx.Frame):
+    """
+    A Frame that says Hello World
+    """
 
-        # Set up the GUI part
-        self.mainGui = Ui(self, app)
+    def __init__(self, *args, **kw):
+        # ensure the parent's __init__ is called
+        super(HelloFrame, self).__init__(*args, **kw)
+
+        self.SetSize((1280, 720))
+        # and create a sizer to manage the layout of child widgets
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.browser = wx.html2.WebView.New(self)
+        sizer.Add(self.browser, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+        # create a menu bar
+        self.makeMenuBar()
+
+        # and a status bar
+        self.CreateStatusBar()
+        self.SetStatusText("Welcome to wxPython!")
+
+    def makeMenuBar(self):
+        """
+        A menu bar is composed of menus, which are composed of menu items.
+        This method builds a set of menus and binds handlers to be called
+        when the menu item is selected.
+        """
+
+        # Make a file menu with Hello and Exit items
+        fileMenu = wx.Menu()
+        # The "\t..." syntax defines an accelerator key that also triggers
+        # the same event
+        loadItem = fileMenu.Append(-1, "&Load...\tCtrl-L",
+                "Load a GeoJSON file")
+        fileMenu.AppendSeparator()
+        # When using a stock ID we don't need to specify the menu item's
+        # label
+        exitItem = fileMenu.Append(wx.ID_EXIT)
+
+        # Now a help menu for the about item
+        helpMenu = wx.Menu()
+        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+        # Make the menu bar and add the two menus to it. The '&' defines
+        # that the next letter is the "mnemonic" for the menu item. On the
+        # platforms that support it those letters are underlined and can be
+        # triggered from the keyboard.
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        # Give the menu bar to the frame
+        self.SetMenuBar(menuBar)
+
+        # Finally, associate a handler function with the EVT_MENU event for
+        # each of the menu items. That means that when that menu item is
+        # activated then the associated handler function will be called.
+        self.Bind(wx.EVT_MENU, self.OnLoad, loadItem)
+        self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
+        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+
+    def OnExit(self, event):
+        """Close the frame, terminating the application."""
+        self.Close(True)
+
+    def OnLoad(self, event):
+        """Say hello to the user."""
+        wx.MessageBox("Not implemented yet")
+
+    def OnAbout(self, event):
+        """Display an About Dialog"""
+        wx.MessageBox("Version: 1.0 (development state)"
+                      "\nGitHub : https://github.com/noero/Territory_Editor.git"
+                      "\nCreator: Romain Damiano",
+                      "About Territory Editor",
+                      wx.OK|wx.ICON_INFORMATION)
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    client = ThreadedClient(root)
-    root.mainloop()
+    app = wx.App()
+    frm = HelloFrame(None, title='Territory Editor')
+    m = folium.Map(
+        location=[48.559400, 7.683222], tiles="OpenStreetMap", zoom_start=13
+    )
+    draw = plugins.Draw(draw_options={'polyline': False,
+                                      'rectangle': False,
+                                      'circle': False,
+                                      'marker': False,
+                                      'circlemarker': False})
+    draw.add_to(m)
+    data = io.BytesIO()
+    m.save(data, close_file=False)
+    frm.browser.SetPage(data.getvalue().decode(), 'https://noero.fr')
+    frm.Show()
+    app.MainLoop()
