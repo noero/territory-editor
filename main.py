ceed18be7aa6300939d4934b9ed9aed68ca5a154
Romain Damiano <rdamianotravail@gmail.com>
2022-05-30 19:04:37 +0200

    First try of load function

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python
 import io
-import folium
-from folium import plugins
 import wx
+import folium
 import wx.html2
+from folium import plugins
 
 
-class HelloFrame(wx.Frame):
-    """
-    A Frame that says Hello World
-    """
-
+class MapFrame(wx.Frame):
     def __init__(self, *args, **kw):
-        # ensure the parent's __init__ is called
-        super(HelloFrame, self).__init__(*args, **kw)
+        super(MapFrame, self).__init__(*args, **kw)
 
         self.SetSize((1280, 720))
-        # and create a sizer to manage the layout of child widgets
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.browser = wx.html2.WebView.New(self)
         sizer.Add(self.browser, 1, wx.EXPAND)
         self.SetSizer(sizer)
 
-        # create a menu bar
         self.makeMenuBar()
-
-        # and a status bar
         self.CreateStatusBar()
-        self.SetStatusText("Welcome to wxPython!")
+        self.SetStatusText("")
 
     def makeMenuBar(self):
         """
@@ -35,50 +26,61 @@ class HelloFrame(wx.Frame):
         This method builds a set of menus and binds handlers to be called
         when the menu item is selected.
         """
-
-        # Make a file menu with Hello and Exit items
         fileMenu = wx.Menu()
-        # The "\t..." syntax defines an accelerator key that also triggers
-        # the same event
         loadItem = fileMenu.Append(-1, "&Load...\tCtrl-L",
-                "Load a GeoJSON file")
+                                   "Load a GeoJSON file")
         fileMenu.AppendSeparator()
-        # When using a stock ID we don't need to specify the menu item's
-        # label
         exitItem = fileMenu.Append(wx.ID_EXIT)
 
-        # Now a help menu for the about item
         helpMenu = wx.Menu()
-        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+        aboutItem = helpMenu.Append(wx.ID_ABOUT, "", " ")
 
-        # Make the menu bar and add the two menus to it. The '&' defines
-        # that the next letter is the "mnemonic" for the menu item. On the
-        # platforms that support it those letters are underlined and can be
-        # triggered from the keyboard.
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu, "&File")
         menuBar.Append(helpMenu, "&Help")
-
-        # Give the menu bar to the frame
         self.SetMenuBar(menuBar)
 
-        # Finally, associate a handler function with the EVT_MENU event for
-        # each of the menu items. That means that when that menu item is
-        # activated then the associated handler function will be called.
         self.Bind(wx.EVT_MENU, self.OnLoad, loadItem)
         self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
         self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
 
     def OnExit(self, event):
-        """Close the frame, terminating the application."""
         self.Close(True)
 
     def OnLoad(self, event):
-        """Say hello to the user."""
-        wx.MessageBox("Not implemented yet")
+        if wx.MessageBox("Current content will be overwritten! Proceed?", "Please confirm",
+                         wx.ICON_QUESTION | wx.YES_NO, self) == wx.NO:
+            return
+
+        with wx.FileDialog(self, "Open GeoJSON file", wildcard="GeoJSON files (*.json)|*.json",
+                           style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return
+
+            # Proceed loading the file chosen by the user
+            pathname = fileDialog.GetPath()
+            try:
+                m = folium.Map(
+                    location=[48.559400, 7.683222], tiles="OpenStreetMap", zoom_start=13
+                )
+                # TODO parse JSON to create polygons and add them to map
+                folium.GeoJson(pathname, name=pathname.split('/')[-1].split('.json')[0].capitalize()).add_to(m)
+                folium.LayerControl().add_to(m)
+                draw = plugins.Draw(draw_options={'polyline': False,
+                                                  'rectangle': False,
+                                                  'circle': False,
+                                                  'marker': False,
+                                                  'circlemarker': False},
+                                    export=True)
+                draw.add_to(m)
+                data = io.BytesIO()
+                m.save(data, close_file=False)
+                frm.browser.SetPage(data.getvalue().decode(), '')
+            except IOError:
+                wx.LogError("Cannot open file '%s'." % pathname)
 
     def OnAbout(self, event):
-        """Display an About Dialog"""
         wx.MessageBox("Version: 1.0 (development state)"
                       "\nGitHub : https://github.com/noero/Territory_Editor.git"
                       "\nCreator: Romain Damiano",
@@ -88,16 +90,10 @@ class HelloFrame(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    frm = HelloFrame(None, title='Territory Editor')
+    frm = MapFrame(None, title='Territory Editor')
     m = folium.Map(
         location=[48.559400, 7.683222], tiles="OpenStreetMap", zoom_start=13
     )
-    draw = plugins.Draw(draw_options={'polyline': False,
-                                      'rectangle': False,
-                                      'circle': False,
-                                      'marker': False,
-                                      'circlemarker': False})
-    draw.add_to(m)
     data = io.BytesIO()
     m.save(data, close_file=False)
     frm.browser.SetPage(data.getvalue().decode(), 'https://noero.fr')
